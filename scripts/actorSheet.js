68e7ff167e8e5a5a0ef61ab060e136ce2ce2b786
bkurtisadams <briankurtisadams@gmail.com>
2025-03-19 22:22:51 -0500

    browse compendium button now works

--- scripts/actorSheet.js
+++ scripts/actorSheet.js
@@ -29,4 +29,31 @@ export class FaseripActorSheet extends ActorSheet {
     // Call the parent update
     return super._updateObject(event, expandedData);
   }
+
+  // In actorSheet.js, add to the activateListeners function
+activateListeners(html) {
+  super.activateListeners(html);
+  
+  // Add Power button
+  html.find('.add-power').click(async ev => {
+    const itemData = {
+      name: "New Power",
+      type: "power",
+      img: "icons/svg/lightning.svg" // Default icon for powers
+    };
+    await this.actor.createEmbeddedDocuments("Item", [itemData]);
+  });
+  
+  // Browse Powers Compendium button
+  html.find('.browse-compendium[data-type="powers"]').click(ev => {
+    const pack = game.packs.find(p => p.metadata.name === "powers" && p.metadata.system === "msh-faserip");
+    if (pack) {
+      pack.render(true);
+    } else {
+      ui.notifications.warn("Powers compendium not found.");
+    }
+  });
+  
+  // Continue with other listeners...
+}
 }
